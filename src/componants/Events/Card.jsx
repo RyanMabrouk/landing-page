1d5b828e4b3068a1b72008c310e2e2f64ebac86a
Rayen Mabrouk <mabroukr911@gmail.com>
2023-10-12 20:44:17 +0100

    fixed the font

--- src/componants/Events/Card.jsx
+++ src/componants/Events/Card.jsx
@@ -4,7 +4,7 @@ import watch from "../../assets/_Events/watch.svg";
 export default function Card(props) {
   return (
     <div className="card">
-      <img src={props.bg} alt="" />
+      <img className="card_img" src={props.bg} alt="" />
       <div className="card_details">
         <h3>{props.name}</h3>
         <img src={watch} alt="" />
